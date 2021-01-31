876167c0f92f3f8c4fb0df74223c5bbbff6470d3
ingyu1008 <ingyu1008@gmail.com>
2021-01-31 16:41:06 +0900

    test

--- test/blog/src/main/java/io/github/ingyu1008/servlet/FirstServlet.java
+++ test/blog/src/main/java/io/github/ingyu1008/servlet/FirstServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/")
+@WebServlet("/test/blog")
 public class FirstServlet extends HttpServlet {
 
     /**
